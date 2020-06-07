5d7ed780207e058ec3cd32e184ed5ceed1ae5be7
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-06-07 18:28:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const testVar = {}
 
-function testFunc() {
-  return "hi"
+function superbowlWin(arrOfObjs) {
+  let finded = arrOfObjs.find(element => element.result === "W")
+  return finded !== undefined ? finded.year : finded
 }
