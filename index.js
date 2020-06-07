22113dc288aa4170afb831b03c8dc958cf377c3c
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-06-07 17:53:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function testFunc() {
 
 let superbowlWin = (arrOfObjs) => {
   let result = arrOfObjs.filter(obj => obj.result === "W")
-  result === -1 ? 'undefined' : result.year
+  result === -1 ? 'undefined' : result[0].year
 }
